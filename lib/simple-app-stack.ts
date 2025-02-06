2f85cb35ebcfa19c60bdfa76062d73c5e5d1fb4c
Justin-SETU <20110497@mail.wit.ie>
2025-02-06 16:08:58 +0000

    Simple standalone lambda

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
+      architecture: lambda.Architecture.ARM_64,  // Uses ARM for cost-efficiency
+      runtime: lambda.Runtime.NODEJS_22_X,       // Node.js 22 runtime
+      entry: `${__dirname}/../lambdas/simple.ts`, // Entry point for Lambda
+      timeout: cdk.Duration.seconds(10),         // Timeout of 10 seconds
+      memorySize: 128,                            // Allocated 128 MB memory
+    });
   }
 }
