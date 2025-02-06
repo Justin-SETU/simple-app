3035c83d247e0fcfe9ba27e0043d447ea9e97f17
Justin-SETU <20110497@mail.wit.ie>
2025-02-06 17:41:41 +0000

    Lambda function URLs.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-
 import { Construct } from 'constructs';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Define the Lambda function with ARM architecture and Node.js 22
     const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
       architecture: lambda.Architecture.ARM_64,  // Uses ARM for cost-efficiency
       runtime: lambda.Runtime.NODEJS_22_X,       // Node.js 22 runtime
@@ -15,5 +15,17 @@ export class SimpleAppStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(10),         // Timeout of 10 seconds
       memorySize: 128,                            // Allocated 128 MB memory
     });
+
+    // Add Function URL (Public Access)
+    const simpleFnURL = simpleFn.addFunctionUrl({
+      // authType: lambda.FunctionUrlAuthType.NONE, // Public access
+      authType: lambda.FunctionUrlAuthType.AWS_IAM, // Protected Access
+      cors: {
+        allowedOrigins: ["*"], // Allow all origins
+      },
+    });
+
+    // Output the function URL
+    new cdk.CfnOutput(this, "SimpleFunctionUrl", { value: simpleFnURL.url });
   }
 }
